8998a71a9e2f8e859b7baa00c3af99291f09f15b
webdreamer25 <edgarolivares29@yahoo.com>
2019-02-17 17:22:36 -0600

    adding a test colorblock module.

--- src/app/app.entry.js
+++ src/app/app.entry.js
@@ -1,9 +1,11 @@
 import HeaderModule from './modules/header.module';
 // import MainModule from './modules/main.module';
+import ColorBlockModule from './modules/colorblock.module';
 import CarHubModule from './modules/car-hub.module';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 HeaderModule.render();
 // MainModule.render();
+ColorBlockModule.render();
 CarHubModule.render();
