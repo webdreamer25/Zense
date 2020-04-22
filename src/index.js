4f14c8482c2b3aa310b560929529c86581fe18fc
webdreamer25 <edgarolivares29@yahoo.com>
2020-04-22 17:14:22 -0500

    added new zense build file and changed versioning.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const Zense = {
   Module
 };
 
-Zense.VERSION = '1.4.8';
+Zense.VERSION = '1.5.0';
 
 // Export Zense object for **Node.js**, with
 // backwards-compatibility for their old module API. 
